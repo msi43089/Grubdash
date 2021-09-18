8612d765ee011f179bdd90351870dabd932dea8d
msi43089 <msi43089@gmail.com>
2021-09-17 21:22:48 -0400

    Refactor Orders

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -9,70 +9,70 @@ const nextId = require("../utils/nextId");
 // TODO: Implement the /orders handlers needed to make the tests pass
 
 function validateDeliverTo(req,res, next){
-  const { data: { deliverTo } = {} } = req.body
+  const { data: { deliverTo } = {} } = req.body;
   if(deliverTo){
-    next()
+    next();
   } else {
-    next({status: 400, message: "Order must include a deliverTo"})
+    next({status: 400, message: "Order must include a deliverTo"});
   }
 }
 
 function validateMobile(req,res, next){
-  const { data: { mobileNumber } = {} } = req.body
+  const { data: { mobileNumber } = {} } = req.body;
   if(mobileNumber){
-    next()
+    next();
   } else {
-    next({status: 400, message: "Order must include a mobileNumber"})
+    next({status: 400, message: "Order must include a mobileNumber"});
   }
 }
 
 function validateDish(req, res, next) {
-  const{ data:  { dishes } = {} } = req.body
+  const{ data: { dishes } = {} } = req.body;
   if(dishes){
-    next()
+    next();
   } else {
-    next({status: 400, message: "Order must include a dish"})
+    next({status: 400, message: "Order must include a dish"});
   }
 }
 
 function validateDishes(req, res, next) {
-  const{ data:  { dishes } = {} } = req.body
+  const{ data:  { dishes } = {} } = req.body;
   if(Array.isArray(dishes) && dishes.length > 0){
-    next()
+    next();
   } else {
-    next({status: 400, message: "Order must include at least one dish"})
+    next({status: 400, message: "Order must include at least one dish"});
   }
 }
 
 function validateQuantity(req, res, next){
-    const{ data:  { dishes } = {} } = req.body
+    const{ data:  { dishes } = {} } = req.body;
     const index = dishes.findIndex((dish) => {
-      return (!dish.quantity || dish.quantity <= 0 || !Number.isInteger(dish.quantity))})
+      return (!dish.quantity || dish.quantity <= 0 || !Number.isInteger(dish.quantity))});
     if(index > -1){
-      next({status: 400, message: `Dish ${index} must have a quantity that is an integer greater than 0`})
+      next({status: 400, message: `Dish ${index} must have a quantity that is an integer greater than 0`});
     } else {
-      next()
+      next();
     }
 }
 
 function checkId(req, res, next){
     const { orderId } = req.params;
-    const foundOrder = orders.find((order) => order.id === orderId)
+    const foundOrder = orders.find((order) => order.id === orderId);
     if(foundOrder){
-      res.locals.foundUrl = foundOrder
+      res.locals.orderId = orderId;
+      res.locals.foundOrder = foundOrder;
       next();
     } else {
-      next({status: 404, message: `Order does not exist: ${orderId}`})
+      next({status: 404, message: `Order does not exist: ${orderId}`});
     }
  }
 
  function matchId(req,res,next){
-    const { orderId } = req.params;
-    const { data: { id } = {} } = req.body
-    if(id && id !== orderId){
-      next({status: 400, message: `Order id does not match route id. Order: ${id}, Route: ${orderId}`})
+    const { data: { id } = {} } = req.body;
+    if(id && id !== res.locals.orderId){
+      next({status: 400, message: `Order id does not match route id. Order: ${id}, Route: ${res.locals.orderId}`});
     } else {
-      next()
+      next();
     }
   }
 
@@ -82,76 +82,73 @@ function checkId(req, res, next){
          status === "delivered" || 
          status === "out-for-delivery" ||
          status === "preparing"){
-        next()
+        next();
       } else {
-        next({status: 400, message: `Order must have a status of pending, preparing, out-for-delivery, delivered`})
+        next({status: 400, message: `Order must have a status of pending, preparing, out-for-delivery, delivered`});
       }
   }
 
   function validatePending(req, res, next) {
-    const { orderId } = req.params
-    const foundOrder = orders.find((order) => order.id === orderId)
-    if(foundOrder.status === "pending"){
-      next()
+    if(res.locals.foundOrder.status === "pending"){
+      next();
     } else {
-      next({status: 400, message: `An order cannot be deleted unless it is pending`})
+      next({status: 400, message: `An order cannot be deleted unless it is pending`});
     }
   }
 
 function list (req, res, next) {
-    res.json({data: orders})
+    res.json({data: orders});
 }
 
 function read(req, res, next) {
-  const { orderId } = req.params
-  const foundOrder = orders.find((order) => order.id === orderId);
-  res.json({data: foundOrder})
+  res.json({data: res.locals.foundOrder});
 }
 
 function update(req, res, next) {
-  const { orderId } = req.params
-  const { data: order = {} } = req.body
-  let foundOrder = orders.find((order) => order.id === orderId);
-  foundOrder = {...order, id: orderId}
-  res.json({data: foundOrder})
+  const { data: order = {} } = req.body;
+  res.locals.foundOrder = {...order, id: res.locals.orderId}
+  res.json({data: res.locals.foundOrder});
  
 }
 
 function destroy(req, res, next) {
-  const { orderId } = req.params
-  let index = orders.findIndex((order) => order.id === orderId);
-  orders.splice(index,1)
-  res.sendStatus(204)
+  let index = orders.findIndex((order) => order.id === res.locals.orderId);
+  orders.splice(index,1);
+  res.sendStatus(204);
 }
 
 function create(req, res, next) {
-  const { data: order = {} } = req.body
-  const newId = nextId()
+  const { data: order = {} } = req.body;
+  const newId = nextId();
   const newOrder = {...order, id: newId}
-  orders.push(newOrder)
-  res.status(201).json({data: newOrder})
+  orders.push(newOrder);
+  res.status(201).json({data: newOrder});
 }
 
 module.exports = {
   list,
-  read: [checkId, read],
-  create: [validateDeliverTo,
-           validateMobile, 
-           validateDish, 
-           validateDishes, 
-           validateQuantity, 
-           create],
-  delete: [checkId,
-           validatePending,
-    destroy],
+  read: [
+        checkId,
+        read],
+  create: [
+        validateDeliverTo,
+        validateMobile, 
+        validateDish, 
+        validateDishes, 
+        validateQuantity, 
+        create],
+  delete: [
+        checkId,
+        validatePending,
+        destroy],
   update: [
-      checkId, 
-      validateDeliverTo, 
-      validateMobile, 
-      validateDish, 
-      validateDishes, 
-      validateQuantity, 
-      validateStatus,
-      matchId,  
-      update]
+        checkId, 
+        validateDeliverTo, 
+        validateMobile, 
+        validateDish, 
+        validateDishes, 
+        validateQuantity, 
+        validateStatus,
+        matchId,  
+        update]
 }
