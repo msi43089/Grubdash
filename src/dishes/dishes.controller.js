d739d9ecd8686e08eca95d8d6310f02e77864d2f
msi43089 <msi43089@gmail.com>
2021-09-17 21:29:12 -0400

    Refactor Dishes

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -10,102 +10,114 @@ const nextId = require("../utils/nextId");
 
 function checkId(req, res, next){
    const { dishId } = req.params;
-   const foundDish = dishes.find((dish) => dish.id === dishId)
+   const foundDish = dishes.find((dish) => dish.id === dishId);
    if(foundDish){
-     res.locals.foundUrl = foundDish
+     res.locals.dishId = dishId
+     res.locals.foundDish = foundDish
      next();
    } else {
-     next({status: 404, message: `Dish does not exist: ${dishId}`})
+     next({status: 404, message: `Dish does not exist: ${dishId}`});
    }
 }
 
 function matchId(req,res,next){
-  const { dishId } = req.params;
-  const { data: { id } = {} } = req.body
-  if(id && id !== dishId){
-    next({status: 400, message: `Dish id does not match route id. Dish: ${id}, Route: ${dishId}`})
+  const { data: { id } = {} } = req.body;
+  if(id && id !== res.locals.dishId){
+    next({status: 400, message: `Dish id does not match route id. Dish: ${id}, Route: ${res.locals.dishId}`});
   } else {
-    next()
+    next();
   }
 }
 
 function validateName(req, res, next){
-    const { data: { name } ={} } = req.body
+    const { data: { name } ={} } = req.body;
     if(!name){
-        next({status: 400, message: "Dish must incluce a name"})
+        next({status: 400, message: "Dish must incluce a name"});
     }
     else {
-        next()
+        next();
     }
 }
 
 function validateDescription(req, res, next){
-    const { data: { description } ={} } = req.body
+    const { data: { description } ={} } = req.body;
     if(!description){
-        next({status: 400, message: "Dish must include a description"})
+        next({status: 400, message: "Dish must include a description"});
     }
     else {
-        next()
+        next();
     }
 }
 
 function validatePrice(req, res, next){
-    const { data: { price } ={} } = req.body
+    const { data: { price } ={} } = req.body;
     if(price <= 0 || !Number.isInteger(price)){
-        next({status: 400, message: "Dish must have a price that is an integer greater than 0"})
+        next({status: 400, message: "Dish must have a price that is an integer greater than 0"});
     }
     else if(!price) {
-        next({status: 400, message: "Dish must include a price"})
+        next({status: 400, message: "Dish must include a price"});
     }
     else {
-        next()
+        next();
     }
 }
 
 function validateImg(req, res, next){
-    const { data: { image_url } ={} } = req.body
+    const { data: { image_url } ={} } = req.body;
     if(!image_url){
-        next({status: 400, message: "Dish must include a image_url"})
+        next({status: 400, message: "Dish must include a image_url"});
     }
     else {
-        next()
+        next();
     }
 }
 
 function create(req, res, next) {
-  const { data: dish = {} } = req.body
-  const newId = nextId()
+  const { data: dish = {} } = req.body;
+  const newId = nextId();
   let newDish = {...dish,
     id: newId   
   }
-  dishes.push(newDish)
-  res.status(201).json({data: newDish})
+  dishes.push(newDish);
+  res.status(201).json({data: newDish});
 }
 
 function read(req, res, next){
-    const { dishId } = req.params
-    const foundDish = dishes.find((dish) => dish.id === dishId)
-    res.json({data: foundDish})
+    res.json({data: res.locals.foundDish});
 
 }
 
 function update (req, res, next) {
-    const { data: dish = {} } = req.body
-    const dishId = Number(req.params.dishId)
-    let foundDish = dishes.find((dish) => dishId === dish.id);
-    foundDish = {...dish, id: `${dishId}`}
-    res.json({data: foundDish})
+    const { data: dish = {} } = req.body;
+    res.locals.foundDish = {...dish, id: `${res.locals.dishId}`}
+    res.json({data: res.locals.foundDish});
 }
 
 function list (req, res, next) {
-    res.json({data: dishes})
+    res.json({data: dishes});
 }
 
 
 
 module.exports = {
-        read: [checkId, read],
-        update: [checkId, matchId, validateName, validateDescription, validateImg, validateImg, validatePrice, update],
+        read: [
+          checkId, 
+          read],
+        update: [
+          checkId, 
+          matchId, 
+          validateName, 
+          validateDescription, 
+          validateImg, 
+          validateImg, 
+          validatePrice, 
+          update],
         list,
-        create: [validateName, validateDescription, validateImg, validateImg, validatePrice, create ]
+        create: [
+          validateName, 
+          validateDescription, 
+          validateImg, 
+          validateImg, 
+          validatePrice, 
+          create ]
 }
